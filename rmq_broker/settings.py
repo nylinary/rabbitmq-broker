5e77d30c4efbb3cf39404e7ec4abed5564dc9044
Сысоев Никита Сергеевич <n.sisoev@energy.digital>
2023-02-16 17:31:13 +0300

    Default paths to settings

--- rmq_broker/settings.py
+++ rmq_broker/settings.py
@@ -3,11 +3,21 @@ import importlib
 import environ
 
 env = environ.Env()
-settings_path = env("MICROSERVICE_SETTINGS", default="config.settings.base")
+paths = {
+    "Django": "config.settings.base",
+    "FastAPI": "app.settings",
+    }
+
+
 
 try:
+    settings_path = env("MICROSERVICE_SETTINGS", default=paths["Django"])
     settings = importlib.import_module(settings_path)
 except ModuleNotFoundError as e:
-    raise AttributeError(
-        "Specified microservice settings file path is not correct! Error: %s" % e
-    )
+    try:
+        settings_path = env("MICROSERVICE_SETTINGS", default=paths["FastAPI"])
+        settings = importlib.import_module(settings_path)
+    except ModuleNotFoundError as e:
+        raise AttributeError(
+            "Specified microservice settings file path is not correct! Error: %s" % e
+        )   
